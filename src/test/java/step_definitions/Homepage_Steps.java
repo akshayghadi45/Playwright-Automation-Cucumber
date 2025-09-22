59c638a2ecdba1462be023600fe8d17f7827ef36
akshayghadi <akshayghadi45@gmail.com>
2025-09-22 18:49:14 +0530

    use the homePage object in Homepage_Steps

--- src/test/java/step_definitions/Homepage_Steps.java
+++ src/test/java/step_definitions/Homepage_Steps.java
@@ -3,6 +3,7 @@ package step_definitions;
 import browser.BrowserManager;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
+import pages.HomePage;
 import pages.base.BasePage;
 
 public class Homepage_Steps {
@@ -26,34 +27,39 @@ public class Homepage_Steps {
 
     BrowserManager browserManager;
     BasePage basePage;
+    HomePage homePage;
 
-    public  Homepage_Steps(BrowserManager browserManager,  BasePage basePage) {
+    public  Homepage_Steps(BrowserManager browserManager,  BasePage basePage, HomePage homePage) {
         this.browserManager=browserManager;
         this.basePage=basePage;
+        this.homePage=homePage;
     }
 
     @Given("I navigate to webdriveruniversity homepage")
     public void i_navigate_to_webdriveruniversity_homepage() {
-       basePage.navigate("https://www.webdriveruniversity.com");
+       //basePage.navigate("https://www.webdriveruniversity.com");
+        homePage.NavigateToHomePage();
     }
     @When("I click on the contact us button")
     public void i_click_on_the_contact_us_button() {
 
-        browserManager.setPage( browserManager.getBrowserContext().waitForPage(()->{
-            //browserManager.getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("CONTACT US Contact Us Form")).click();
-            //Commented above code to make use of the base class
-            basePage.waitAndClickByRole("LINK","CONTACT US Contact Us Form");
-        }));
-        browserManager.getPage().bringToFront();
+//        browserManager.setPage( browserManager.getBrowserContext().waitForPage(()->{
+//            //browserManager.getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("CONTACT US Contact Us Form")).click();
+//            //Commented above code to make use of the base class
+//            basePage.waitAndClickByRole("LINK","CONTACT US Contact Us Form");
+//        }));
+//        browserManager.getPage().bringToFront();
+        homePage.clickContactUsButton();
 
     }
     @When("I click on the login portal button")
     public void i_click_on_the_login_portal_button() {
-        browserManager.setPage( browserManager.getBrowserContext().waitForPage(()->{
-            //browserManager.getPage().locator("#login-portal").click();
-            basePage.waitAndClickByRole("LINK","LOGIN PORTAL Login Portal");
-        }));
-        browserManager.getPage().bringToFront();
+//        browserManager.setPage( browserManager.getBrowserContext().waitForPage(()->{
+//            //browserManager.getPage().locator("#login-portal").click();
+//            basePage.waitAndClickByRole("LINK","LOGIN PORTAL Login Portal");
+//        }));
+//        browserManager.getPage().bringToFront();
+        homePage.clickLoginButton();
     }
 
 }
